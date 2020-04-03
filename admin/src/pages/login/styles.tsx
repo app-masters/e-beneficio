612ea806d69d6ebb7af587234ecf02576c97874a
Baraky <jfbaraky@gmail.com>
2020-04-03 17:12:17 -0300

    feat: Login page, login actions and conditional routing

---
+++ admin/src/pages/login/styles.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const FormContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
--- admin/src/redux/auth/reducers.ts
+++ admin/src/redux/auth/reducers.ts
@@ -1,6 +1,6 @@
 import { createReducer } from '@reduxjs/toolkit';
 import * as TYPES from './types';
-import { LoginUserSuccessAction, LoginUserFailedAction } from './actions';
+import { LoginUserSuccessAction, LoginUserFailedAction, GetTokenSuccessAction, GetTokenFailedAction } from './actions';
 import { User } from '../../interfaces/user';
 
 export interface AuthReducerState {
@@ -29,5 +29,21 @@ export default createReducer<AuthReducerState>(initialState, {
   [TYPES.LOGIN_USER_FAILED]: (state: AuthReducerState, action: LoginUserFailedAction) => {
     state.loading = false;
     state.error = action.error;
+  },
+  [TYPES.GET_TOKEN]: (state: AuthReducerState) => {
+    state.loading = true;
+  },
+  [TYPES.GET_TOKEN_SUCCESS]: (state: AuthReducerState, action: GetTokenSuccessAction) => {
+    state.loading = false;
+    state.token = action.response.token;
+    state.refreshToken = action.response.refreshToken;
+    state.user = action.response.user;
+  },
+  [TYPES.GET_TOKEN_FAILED]: (state: AuthReducerState) => {
+    state.loading = false;
+    // Token failed, log out the user
+    state.token = undefined;
+    state.refreshToken = undefined;
+    state.user = undefined;
   }
 });
