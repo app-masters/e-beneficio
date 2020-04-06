5d5fccd16f8247f7c4dced952d49dee50da3ca51
Matheus Assis <assisr.matheus@gmail.com>
2020-04-06 16:18:22 -0300

    feat: #28 - added more logic to refresh token

--- admin/src/components/AdminLayout/index.tsx
+++ admin/src/components/AdminLayout/index.tsx
@@ -1,22 +1,35 @@
-import { Layout } from 'antd';
+import { Spin, Layout } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
 import { Sidebar } from '../Sidebar';
+import { SpinContainer } from './styles';
 const { Content } = Layout;
 
 const Wrapper = styled(Layout)`
   height: 100%;
 `;
 
+type AdminLayoutProps = {
+  loading?: boolean;
+};
+
 /**
  * The admin layout. Which defines the default sidebar with content in its middle section
  */
-export const AdminLayout: React.FC = (props) => {
+export const AdminLayout: React.FC<AdminLayoutProps> = (props) => {
   return (
     <Wrapper>
       <Sidebar />
       <Layout>
-        <Content>{props.children}</Content>
+        <Content>
+          {props.loading ? (
+            <SpinContainer>
+              <Spin />
+            </SpinContainer>
+          ) : (
+            props.children
+          )}
+        </Content>
       </Layout>
     </Wrapper>
   );
---
+++ admin/src/components/AdminLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const SpinContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
