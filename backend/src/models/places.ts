da92e328ed159795fdf49fdbace79c5ae0271744
Baraky <jfbaraky@gmail.com>
2020-04-07 09:50:23 -0300

    feat: Handling city on the place routes

--- backend/src/models/places.ts
+++ backend/src/models/places.ts
@@ -1,12 +1,14 @@
 import db from '../schemas';
 import { Place, SequelizePlace } from '../schemas/places';
+import { User } from '../schemas/users';
 
 /**
  * Get all items on the table without any filter
+ * @param cityId requested city ID
  * @returns Promise<List of items>
  */
-export const getAll = (): Promise<SequelizePlace[]> => {
-  return db.places.findAll();
+export const getAll = (cityId: number): Promise<SequelizePlace[]> => {
+  return db.places.findAll({ where: { cityId } });
 };
 
 /**
@@ -45,4 +47,19 @@ export const updateById = async (id: string | number, values: Place | SequelizeP
  */
 export const deleteById = (id: string | number): void => {
   db.places.destroy({ where: { id } });
+};
+
+/**
+ * Check if item belongs tot he user city
+ * @param id unique ID of the desired item
+ * @param user user ID
+ */
+export const checkItemCity = async (id: string | number, user?: User): Promise<SequelizePlace> => {
+  // Check user
+  if (!user || !user.cityId) throw { status: 401, message: 'Invalid request' };
+  // Request item
+  const item = await getById(id);
+  // Throw error if item is from another city
+  if (!item || item.cityId !== user.cityId) throw { status: 404, message: 'Not found' };
+  return item;
 };
