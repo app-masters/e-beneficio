ebf5737b20e273434812ebe6da305c91c6a77cb2
Alessandro Macanha <alessandromacanha@gmail.com>
2020-04-13 15:48:02 -0300

    added: initial dashboard design

--- public/src/pages/dashboard/styles.tsx
+++ public/src/pages/dashboard/styles.tsx
@@ -1,6 +1,36 @@
 import styled from 'styled-components';
 
+export const InputStyle = {
+  minWidth: window.innerWidth * 0.3
+};
+
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const ActionContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  background-color: #fff;
+`;
+
+export const EstablishmentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
