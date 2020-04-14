de314950fec929edac417894d7df09d1cb00876e
Alessandro Macanha <alessandromacanha@gmail.com>
2020-04-14 09:28:46 -0300

    feat: #65 added collapsable panel info

--- public/src/pages/dashboard/styles.tsx
+++ public/src/pages/dashboard/styles.tsx
@@ -4,9 +4,19 @@ export const InputStyle = {
   minWidth: window.innerWidth * 0.3
 };
 
+export const PanelStyle = {
+  backgroundColor: '#00000000'
+};
+
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const PanelActionContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
 `;
 
 export const ActionContainer = styled.div`
