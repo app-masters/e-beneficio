a454acda511312992ff924d975aab4a087412958
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2020-04-14 14:39:55 -0300

    Updating scripts

--- deployment/deploy-gcp.sh
+++ deployment/deploy-gcp.sh
@@ -21,15 +21,15 @@ gcloud config set compute/zone ${ZONE}
 #echo -e "\n# 2/9 - Pushing docker image to Google Container Registry...\n"
 #docker push ${BACKEND_CONTAINER_IMAGE}
 
-### FRONTEND
-## build and copy admin
-#echo -e "\n# 3/9 - Building admin...\n"
-#cd admin
-#yarn install
-#yarn build
-#mkdir -p ../frontend/admin/
-#cp -R build/ ../frontend/admin/
-#cd ..
+## FRONTEND
+# build and copy admin
+echo -e "\n# 3/9 - Building admin...\n"
+cd admin
+yarn install
+yarn build
+mkdir -p ../frontend/admin/
+cp -R build/ ../frontend/admin/
+cd ..
 
 ## build and copy app
 echo -e "\n# 4/9 - Building app...\n"
@@ -45,17 +45,17 @@ echo -e "\n# 5/9 - Building public...\n"
 cd public
 yarn install
 yarn build
-mkdir -p ../frontend/public/
-cp -R build/ ../frontend/public/
+mkdir -p ../frontend/portal/
+cp -R build/ ../frontend/portal/
 cd ..
 
-## Build docker and so on
-#echo -e "\n# 6/9 - Building and tagging full frontend (admin + app + public) dockerfile...\n"
-#docker build -f deployment/frontend.Dockerfile -t ${FRONTEND_IMAGE_TAG} .
-#docker tag ${FRONTEND_IMAGE_TAG} ${FRONTEND_CONTAINER_IMAGE}
-#
-#echo -e "\n# 7/9 - Pushing docker image to Google Container Registry...\n"
-#docker push ${FRONTEND_CONTAINER_IMAGE}
+# Build docker and so on
+echo -e "\n# 6/9 - Building and tagging full frontend (admin + app + public) dockerfile...\n"
+docker build -f deployment/frontend.Dockerfile -t ${FRONTEND_IMAGE_TAG} .
+docker tag ${FRONTEND_IMAGE_TAG} ${FRONTEND_CONTAINER_IMAGE}
+
+echo -e "\n# 7/9 - Pushing docker image to Google Container Registry...\n"
+docker push ${FRONTEND_CONTAINER_IMAGE}
 
 ### FILES AND FINAL PULL
 echo -e "\n# 8/9 - Copying config files to remote machine...\n"
