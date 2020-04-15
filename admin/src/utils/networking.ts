046be0345e1eccc15f1f1b97a078bb32120aa767
Matheus Assis <assisr.matheus@gmail.com>
2020-04-15 16:42:16 -0300

    feat: #71 - Added token refresh when the token is expired.

--- admin/src/utils/networking.ts
+++ admin/src/utils/networking.ts
@@ -1,28 +1,13 @@
 import axios from 'axios';
-import { TokenResponse } from '../interfaces/auth';
-import { PERSIST_KEY } from './constraints';
-import { AppState } from '../redux/rootReducer';
-
-const reduxPersist = localStorage.getItem(`persist:${PERSIST_KEY}`);
-const parsedReduxPersist = reduxPersist ? JSON.parse(reduxPersist) : undefined;
-const parsedAuthRedux: AppState['authReducer'] | undefined = parsedReduxPersist
-  ? JSON.parse(parsedReduxPersist.authReducer)
-  : undefined;
+import { createAuthAxios } from './auth';
 
 /**
  * Default axios instance
  */
-const backend = axios.create({
-  baseURL: process.env.REACT_APP_ENV_BACKEND_HOST,
-  headers: parsedAuthRedux && parsedAuthRedux.token ? { Authorization: `Bearer ${parsedAuthRedux.token}` } : undefined
-});
+const backend = createAuthAxios(
+  axios.create({
+    baseURL: process.env.REACT_APP_ENV_BACKEND_HOST
+  })
+);
 
-/**
- * Set or clear the default Authorization token on request headers
- * @param token JWT token or null
- */
-const setAuthorization = (token: TokenResponse['token'] | null) => {
-  backend.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
-};
-
-export { backend, setAuthorization };
+export { backend };
