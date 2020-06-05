edd8a043873b9e31849fd7e6b7bc5e12bb51b682
Alessandro Macanha <alessandromacanha@gmail.com>
2020-06-05 09:58:45 -0300

    change year/month to date

--- backend/tests/consumption.test.ts
+++ backend/tests/consumption.test.ts
@@ -27,8 +27,7 @@ let createdFamily: Family;
 const benefit = {
   title: '[CAD25123] Auxilio municipal de alimentação',
   groupName: getFamilyGroupByCode(1)?.key,
-  month: 5,
-  year: 2020,
+  date: moment().toDate(),
   value: 500,
   institutionId: 0
 } as Benefit;
