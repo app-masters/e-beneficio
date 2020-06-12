e359cc525af78b4cbcef32a446534caa3dc99b4b
Alessandro Macanha <alessandromacanha@gmail.com>
2020-06-12 10:49:14 -0300

    removed error from form

--- admin/src/pages/groups/form.tsx
+++ admin/src/pages/groups/form.tsx
@@ -27,7 +27,6 @@ export const GroupForm: React.FC<RouteComponentProps<{ id: string }>> = (props) 
     groupReducer.list.find((f) => f.id === Number(props.match.params.id))
   );
   const loading = useSelector<AppState, boolean>(({ groupReducer }) => groupReducer.loading);
-  const error = useSelector<AppState, Error | undefined>(({ groupReducer }) => groupReducer.error);
 
   const { handleChange, values, getFieldMeta, submitForm, status, errors, touched } = useFormik({
     initialValues: group || {
@@ -51,7 +50,7 @@ export const GroupForm: React.FC<RouteComponentProps<{ id: string }>> = (props) 
       confirmLoading={loading}
       okType={errors && Object.keys(errors).length > 0 && touched ? 'danger' : 'primary'}
     >
-      {(status || error) && <Alert message="Erro no formulário" description={status || error?.message} type="error" />}
+      {status && <Alert message="Erro no formulário" description={status} type="error" />}
       <Form layout="vertical">
         <Form.Item label={'Titulo'} validateStatus={formValidation(titleMeta)} help={formHelper(titleMeta)}>
           <Input id="title" name="title" onChange={handleChange} value={values.title} onPressEnter={submitForm} />
